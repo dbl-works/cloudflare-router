8ed22e727bb6d22dda919d2da8b0649160cd4283
Marc Qualie <marc@marcqualie.com>
2021-10-19 05:43:21 +0100

    Add support for SPA

--- src/utils/normalize-request.ts
+++ src/utils/normalize-request.ts
@@ -1,4 +1,10 @@
-import { Config } from "../config"
+import { Config } from '../config'
+
+const MEDIA_FILE_EXTENSIONS = 'js css jpg png gif woff'.split(' ')
+const hasMediaFileExtension = (path: string): boolean => {
+  const ext = path.split('.').pop().toLowerCase()
+  return MEDIA_FILE_EXTENSIONS.includes(ext)
+}
 
 export default function normalizeRequest(request: Request, routes: Config['routes']): Request {
   const originalUrl = request.url
@@ -6,10 +12,21 @@ export default function normalizeRequest(request: Request, routes: Config['route
   const path = originalUrlWithoutScheme.replace(/^.*?\//gi, '')
 
   for (const [key, value] of Object.entries(routes)) {
-    const lastChar = value[value.length - 1]
     let url = originalUrl
+    let newUrl = value
     if (url.indexOf(key) !== -1) {
-      url = originalUrlWithoutScheme.replace(key, value)
+      const singlePageApp = newUrl.indexOf('s3://') === 0
+      const isMediaFile = hasMediaFileExtension(originalUrl)
+      if (singlePageApp) {
+        newUrl = newUrl.replace(new RegExp('s3://([^.]+).([^/]+)(/?)(.*)'), 'https://s3.$1.amazonaws.com/$2$3$4')
+      }
+
+      const lastChar = newUrl[newUrl.length - 1]
+      url = originalUrlWithoutScheme.replace(key, newUrl)
+      // console.log(path, { singlePageApp, isMediaFile })
+      if (singlePageApp && !isMediaFile) {
+        url = newUrl + '/index.html'
+      }
       if (lastChar === '/') {
         url = url + path
       }
--- test/utils/normalize-request.test.ts
+++ test/utils/normalize-request.test.ts
@@ -3,6 +3,8 @@ import normalizeRequest from '../../src/utils/normalize-request'
 const TEST_ROUTES = {
   'admin.example.com': 'https://s3.eu-central-1.amazonaws.com/assets.example.com/admin',
   'example.com/admin': 'https://s3.eu-central-1.amazonaws.com/assets.example.com/admin',
+  'dashboard.example.com': 's3://eu-central-1.assets.example.com/dashboard',
+  'fonts.example.com': 's3://us-east-1.fonts.example.com',
   'cdn.example.com': 'https://s3.eu-central-1.amazonaws.com/cdn.example.com',
   '/old-path': '/new-path',
 }
@@ -17,6 +19,11 @@ test('maps root js file to s3 bucket subpath', () => {
   expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/admin/some/file.js')
 })
 
+test('maps root path to s3 bucket subpath', () => {
+  const actual = normalizeRequest(new Request('https://admin.example.com/'), TEST_ROUTES)
+  expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/admin/')
+})
+
 test('maps subpath js file to s3 bucket subpath', () => {
   const actual = normalizeRequest(new Request('https://example.com/admin/some/file.js'), TEST_ROUTES)
   expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/admin/some/file.js')
@@ -25,6 +32,26 @@ test('maps subpath js file to s3 bucket subpath', () => {
 test('maps js to s3 bucket root', () => {
   const actual = normalizeRequest(new Request('https://cdn.example.com/some/file.js'), TEST_ROUTES)
   expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/cdn.example.com/some/file.js')
+})
+
+test('maps SPA root path to s3 bucket index', () => {
+  const actual = normalizeRequest(new Request('https://dashboard.example.com/'), TEST_ROUTES)
+  expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/dashboard/index.html')
+})
+
+test('maps SPA sub path to s3 bucket index', () => {
+  const actual = normalizeRequest(new Request('https://dashboard.example.com/users/'), TEST_ROUTES)
+  expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/dashboard/index.html')
+})
+
+test('maps SPA JS FILE to s3 bucket location', () => {
+  const actual = normalizeRequest(new Request('https://dashboard.example.com/some/file.js'), TEST_ROUTES)
+  expect(actual.url).toEqual('https://s3.eu-central-1.amazonaws.com/assets.example.com/dashboard/some/file.js')
+})
+
+test('maps SPA root to s3 bucket root without subpath', () => {
+  const actual = normalizeRequest(new Request('https://fonts.example.com/'), TEST_ROUTES)
+  expect(actual.url).toEqual('https://s3.us-east-1.amazonaws.com/fonts.example.com/index.html')
 })
 
 test('simple path replace', () => {
