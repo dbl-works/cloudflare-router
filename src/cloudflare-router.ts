50ecebdbec9ebfdd16e2dc71d70555853c38fb5b
Sam Kah Chiin <samkahchiin@gmail.com>
2022-09-01 23:27:40 +0800

    [Hotfix] Use request url as cacheKey

--- src/cloudflare-router.ts
+++ src/cloudflare-router.ts
@@ -3,11 +3,6 @@ import normalizeRequest from './utils/normalize-request'
 import { withAuth } from './utils/with-auth'
 
 async function handleRequest(request: Request) {
-  const url = new URL(request.url);
-  // Only use the path for the cache key, removing query strings
-  // and always store using HTTPS, for example, https://www.example.com/file-uri-here
-  const cacheKey = `https://${url.hostname}${url.pathname}`
-
   let response = await fetch(request, {
     // Requests proxied through Cloudflare are cached even without Workers according to a zone’s default
     // or configured behavior. Setting Cloudflare cache rules to further customised the behavior
@@ -15,7 +10,7 @@ async function handleRequest(request: Request) {
     cf: {
       // cf is Enterprise only feature
       cacheTtlByStatus: { '200-299': 86400, '404': 1, '500-599': 0 },
-      cacheKey
+      cacheKey: request.url
     }
   });
   // Reconstruct the Response object to make its headers mutable.
