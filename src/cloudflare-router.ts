5a73a4c905944b7f02fc5db778bcf9e7c0ce0b7c
Sam Kah Chiin <samkahchiin@gmail.com>
2022-09-05 23:40:22 +0800

    [Hotfix] Add edge cache ttl as headers

--- src/cloudflare-router.ts
+++ src/cloudflare-router.ts
@@ -8,14 +8,15 @@ async function handleRequest(request: Request, edgeCacheTtl: number) {
     // or configured behavior. Setting Cloudflare cache rules to further customised the behavior
     cf: {
       // NOTE: cf is Enterprise only feature
-      // This is equivalent to setting two Page Rules: Edge Cache TTL and Cache Level (to Cache Everything).
+      // This is equivalent to setting two Page Rules:
+      // - Edge Cache TTL and
+      // - Cache Level (to Cache Everything).
       // Edge Cache TTL (Time to Live) specifies how long to cache a resource in the Cloudflare edge network
       // The value must be zero or a positive number.
       // A value of 0 indicates that the cache asset expires immediately.
       // This option applies to GET and HEAD request methods only.
       cacheTtlByStatus: { '200-299': edgeCacheTtl, '404': 1, '500-599': 0 },
       // The Cloudflare CDN does not cache HTML by default without setting cacheEverything to true.
-      // See here to understand the risk of setting cacheEverything to true
       cacheEverything: true,
       cacheKey: request.url
     }
@@ -26,7 +27,7 @@ async function handleRequest(request: Request, edgeCacheTtl: number) {
 
   // Reconstruct the Response object to make its headers mutable.
   response = new Response(response.body, response);
-  response.headers.set('squake-router-version', '0.2.19')
+  response.headers.set('edge-cache-ttl', `${edgeCacheTtl}`)
   return response;
 }
 
