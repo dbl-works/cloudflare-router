9b2feb613a50b9f30af4ab50f605b3a46a8b76cd
Sam Kah Chiin <samkahchiin@gmail.com>
2023-07-21 13:17:29 +0800

    [WIP] Bug fix the request headers

--- src/cloudflare-router.ts
+++ src/cloudflare-router.ts
@@ -7,17 +7,12 @@ export const startWorker = (config: Config) => {
   addEventListener('fetch', (event: any) => {
     withAuth(event, config, async () => {
       const { request, cache }= normalizeRequest(event.request, config.routes)
-<<<<<<< HEAD
       const edgeCacheTtl = cache && config.edgeCacheTtl ? config.edgeCacheTtl : 0
-      return handleRequest(request, edgeCacheTtl, origin)
-=======
-      const edgeCacheTtl = cache ? config.edgeCacheTtl : 0
 
       const origin = request.headers.get('origin')
       const headers: Headers | {} = origin ? { origin } : {}
 
       return handleRequest(request, edgeCacheTtl, headers)
->>>>>>> 2f85b1a ([WIP] refactor)
     })
   })
 }
