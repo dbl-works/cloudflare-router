7c194017ada299cc214dcfb9f7db982c8e18c152
marfoldi <peter@dbl.works>
2023-09-26 16:24:58 +0200

    Remove unnecessary `eslint-disable`

--- test/setup.ts
+++ test/setup.ts
@@ -5,7 +5,6 @@ import jestFetchMock from 'jest-fetch-mock'
 jestFetchMock.enableMocks()
 
 // Temporary mocks
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 global.addEventListener = ((eventName: string, listener: (event: EventListenerObject) => void) => {
   listener({
     request: new Request('https://exaple.com/'),
