cfc89cfd900c574ea3099be076443fe693b845a2
Lud <lud@reinmiedl.com>
2026-05-03 21:42:35 +0200

    refactor: make deployments optional in Config (#35)
    Users who only need routing (without auth/deployment matching) no longer
    need to specify 'deployments: []'. Added guards in withAuth and
    deploymentForRequest for undefined deployments.

--- src/utils/deployment-for-request.ts
+++ src/utils/deployment-for-request.ts
@@ -1,7 +1,7 @@
 import { Config, Deployment } from '../config'
 
 export const deploymentForRequest = (request: Request, config: Config): Deployment | undefined => {
-  return config.deployments.find(deployment => {
+  return (config.deployments ?? []).find(deployment => {
     return deployment.routes.find(route => {
       const sanitizedRoute = route.replace(/[^a-zA-Z0-9*\.\-\/]/g, '') // We only really want to allow the pattern *.example.com/*
       const regexedRoute = sanitizedRoute.replace(/\./g, '\\.').replace(/\*/g, '(.*)')
